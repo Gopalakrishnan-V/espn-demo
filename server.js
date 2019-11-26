12925ba2e93980aa2260e15d56493cd999e25f44
gopal221296 <gopalakrishnan.v@unacademy.com>
2019-11-26 22:51:11 +0530

    all endpoints added

--- server.js
+++ server.js
@@ -30,6 +30,25 @@ app.get("/api/teams", async (req, res) => {
   res.send({ data: teams });
 });
 
+app.get("/api/teams/:teamID/:teamSlug/players", async (req, res) => {
+  const { teamID, teamSlug } = req.params;
+  const teamPlayers = await espn.getTeamPlayers({ teamID, teamSlug });
+  res.send({ data: teamPlayers });
+});
+
+app.get("/api/players/:playerID/:teamSlug", async (req, res) => {
+  const { playerID, teamSlug } = req.params;
+  const playerDetails = await espn.getPlayerDetails({ playerID, teamSlug });
+  res.send({ data: playerDetails });
+});
+
+app.get("/api/search", async (req, res) => {
+  let { q, limit } = req.query;
+  limit = parseInt(limit);
+  const searchResults = await espn.search({ query: q, limit });
+  res.send({ data: searchResults });
+});
+
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server started listening on port ${PORT}`);
